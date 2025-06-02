8dad1bdd2a1290ddb3026b7f0d6c76c6be3fb30e
Yaroslav <iaroslav.bierioza@gmail.com>
2025-06-02 12:58:57 +0300

    Initial root route with  Swagger docs URL

--- film-catalog/main.py
+++ film-catalog/main.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    Request,
+)
 
 
 app = FastAPI(
     title="Film Catalog",
 )
+
+
+@app.get("/")
+def read_root(
+    request: Request,
+):
+
+    docs_url = request.url.replace(
+        path="/docs",
+    )
+
+    return {
+        "docs": str(docs_url),
+    }
