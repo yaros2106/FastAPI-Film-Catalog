4304792feef6ff7e5c91b6c0c044958643943baf
Yaroslav <iaroslav.bierioza@gmail.com>
2025-06-11 16:07:49 +0300

    Save state to .json file

--- film-catalog/api/api_v1/films/crud.py
+++ film-catalog/api/api_v1/films/crud.py
@@ -1,10 +1,23 @@
-from pydantic import BaseModel
+from pydantic import (
+    BaseModel,
+    ValidationError,
+)
 
+from core.config import FILM_STORAGE_FILEPATH
 from schemas.film import Film, FilmCreate, FilmUpdate, FilmPartialUpdate
 
 
 class FilmsStorage(BaseModel):
     slug_to_film: dict[str, Film] = {}
+
+    def save_state(self) -> None:
+        return FILM_STORAGE_FILEPATH.write_text(self.model_dump_json(indent=2))
+
+    @classmethod
+    def load_state(cls) -> "FilmsStorage":
+        if not FILM_STORAGE_FILEPATH.exists():
+            return FilmsStorage()
+        return cls.model_validate_json(FILM_STORAGE_FILEPATH.read_text())
 
     def get(self) -> list[Film]:
         return list(self.slug_to_film.values())
@@ -17,10 +30,12 @@ class FilmsStorage(BaseModel):
             **film_crate.model_dump(),
         )
         self.slug_to_film[film.slug] = film
+        self.save_state()
         return film
 
     def delete_by_slug(self, slug: str) -> None:
         self.slug_to_film.pop(slug, None)
+        self.save_state()
 
     def delete(self, film: Film) -> None:
         self.delete_by_slug(slug=film.slug)
@@ -32,6 +47,7 @@ class FilmsStorage(BaseModel):
     ) -> Film:
         for field_name, value in film_in:
             setattr(film, field_name, value)
+        self.save_state()
         return film
 
     def update_partial(
@@ -41,35 +57,12 @@ class FilmsStorage(BaseModel):
     ) -> Film:
         for field_name, value in film_in.model_dump(exclude_unset=True).items():
             setattr(film, field_name, value)
+        self.save_state()
         return film
 
 
-storage = FilmsStorage()
-
-storage.create(
-    FilmCreate(
-        slug="the-walking-dead",
-        title="The Walking Dead",
-        description="A group of survivors navigates a post-apocalyptic world overrun by zombies",
-        year=2010,
-        duration_minutes=44,
-    )
-)
-storage.create(
-    FilmCreate(
-        slug="outcast",
-        title="outcast",
-        description="After a plane crash, a FedEx executive struggles to survive on a deserted island",
-        year=2000,
-        duration_minutes=143,
-    )
-)
-storage.create(
-    FilmCreate(
-        slug="the-matrix",
-        title="The Matrix",
-        description="A computer hacker learns about the true nature of his reality and his role in the war against its controllers",
-        year=1999,
-        duration_minutes=136,
-    )
-)
+try:
+    storage = FilmsStorage().load_state()
+except ValidationError:
+    storage = FilmsStorage()
+    storage.save_state()
---
+++ film-catalog/core/config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+FILM_STORAGE_FILEPATH = BASE_DIR / "films-storage.json"
