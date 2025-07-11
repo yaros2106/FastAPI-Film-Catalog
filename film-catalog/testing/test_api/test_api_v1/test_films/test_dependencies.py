4006f878701a90bec4bbe563fd0f65507fa11615
Yaroslav <iaroslav.bierioza@gmail.com>
2025-07-11 13:24:46 +0300

    pytest class test

--- film-catalog/testing/test_api/test_api_v1/test_films/test_dependencies.py
+++ film-catalog/testing/test_api/test_api_v1/test_films/test_dependencies.py
@@ -1,10 +1,15 @@
 from api.api_v1.films.dependencies import UNSAFE_METHODS
 
 
-def test_unsafe_methods_dont_contain_save_methods() -> None:
-    save_methods = {
-        "GET",
-        "HEAD",
-        "OPTIONS",
-    }
-    assert not UNSAFE_METHODS & save_methods
+class TestUnsafeMethods:
+
+    def test_dont_contain_save_methods(self) -> None:
+        save_methods = {
+            "GET",
+            "HEAD",
+            "OPTIONS",
+        }
+        assert not UNSAFE_METHODS & save_methods
+
+    def test_all_methods_are_upper(self) -> None:
+        assert all(method.isupper() for method in UNSAFE_METHODS)
