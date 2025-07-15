d2cb8940ff462087575cfb77d945804b6e4871a3
Yaroslav <iaroslav.bierioza@gmail.com>
2025-07-15 14:52:28 +0300

    api tests for creation, nested fixtures, scopes

--- film-catalog/testing/test_api/conftest.py
+++ film-catalog/testing/test_api/conftest.py
@@ -3,10 +3,25 @@ from collections.abc import Generator
 import pytest
 from fastapi.testclient import TestClient
 
+from api.api_v1.auth.services import redis_tokens
 from main import app
 
 
 @pytest.fixture()
 def client() -> Generator[TestClient]:
     with TestClient(app) as client:
+        yield client
+
+
+@pytest.fixture(scope="module")
+def auth_token() -> Generator[str]:
+    auth_token = redis_tokens.generate_and_save_token()
+    yield auth_token
+    redis_tokens.delete_token(auth_token)
+
+
+@pytest.fixture(scope="module")
+def auth_client(auth_token: str) -> Generator[TestClient]:
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    with TestClient(app, headers=headers) as client:
         yield client
---
+++ film-catalog/testing/test_api/test_api_v1/test_films/test_views/test_list_views/test_create.py
@@ -0,0 +1,30 @@
+import random
+import string
+
+from fastapi.testclient import TestClient
+from starlette import status
+
+from main import app
+from schemas.film import FilmCreate
+
+
+def test_create_film(auth_client: TestClient) -> None:
+    url = app.url_path_for("create_film")
+    film_in = FilmCreate(
+        slug="".join(
+            random.choices(
+                string.ascii_letters,
+                k=8,
+            ),
+        ),
+        title="title",
+        description="description",
+        year=2000,
+        duration_minutes=100,
+    )
+    data: dict[str, str] = film_in.model_dump()
+    response = auth_client.post(url=url, json=data)
+    assert response.status_code == status.HTTP_201_CREATED, response.text
+    response_data = response.json()
+    received_values = FilmCreate(**response_data)
+    assert received_values == film_in, response_data
