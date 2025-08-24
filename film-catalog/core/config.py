44c92c601b1651764b0ab60833cb3f967f585d29
Yaroslav <iaroslav.bierioza@gmail.com>
2025-08-24 14:04:48 +0300

    logging pydantic-settings

--- film-catalog/core/config.py
+++ film-catalog/core/config.py
@@ -1,18 +1,15 @@
 import logging
-from os import getenv
 from pathlib import Path
 
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-FILM_STORAGE_FILEPATH = BASE_DIR / "films-storage.json"
 
-
-LOG_LEVEL = logging.INFO
 LOG_FORMAT: str = (
     "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
 )
 
-REDIS_HOST = getenv("REDIS_HOST", "localhost")
-REDIS_PORT = int(getenv("REDIS_PORT", "0")) or 6379
 REDIS_DB = 0
 REDIS_DB_TOKENS = 1
 REDIS_DB_USERS = 2
@@ -20,3 +17,27 @@ REDIS_DB_FILMS = 3
 
 REDIS_TOKENS_SET_NAME = "tokens"
 REDIS_FILMS_HASH_NAME = "films"
+
+
+class LoggingConfig(BaseModel):
+    log_level: int = logging.INFO
+    log_format: str = LOG_FORMAT
+    date_format: str = "%Y-%m-%d %H:%M:%S"
+
+
+class RedisConnectionConfig(BaseModel):
+    host: str = "localhost"
+    port: int = 6379
+
+
+class RedisConfig(BaseModel):
+    connection: RedisConnectionConfig = RedisConnectionConfig()
+
+
+class Settings(BaseSettings):
+    logging: LoggingConfig = LoggingConfig()
+    redis: RedisConfig = RedisConfig()
+
+
+# noinspection PyArgumentList
+settings = Settings()
--- film-catalog/main.py
+++ film-catalog/main.py
@@ -7,11 +7,12 @@ from fastapi import (
 from api import router as api_router
 from api.main_views import router as main_router
 from app_lifespan import lifespan
-from core import config
+from core.config import settings
 
 logging.basicConfig(
-    level=config.LOG_LEVEL,
-    format=config.LOG_FORMAT,
+    level=settings.logging.log_level,
+    format=settings.logging.log_format,
+    datefmt=settings.logging.date_format,
 )
 
 app = FastAPI(
