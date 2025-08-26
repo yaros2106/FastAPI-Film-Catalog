2247f2ac5fa0b6d5b088cf4acea916fc889819e8
Yaroslav <iaroslav.bierioza@gmail.com>
2025-08-26 14:06:13 +0300

    setting .env

--- film-catalog/core/config.py
+++ film-catalog/core/config.py
@@ -62,6 +62,9 @@ class RedisConfig(BaseModel):
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         case_sensitive=False,
+        env_file=BASE_DIR / ".env",
+        env_prefix="FILM_CATALOG__",
+        env_nested_delimiter="__",
     )
     logging: LoggingConfig = LoggingConfig()
     redis: RedisConfig = RedisConfig()
