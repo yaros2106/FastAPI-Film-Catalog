b8548b37a1443e0432522f482b9c801c58698ac0
Yaroslav <iaroslav.bierioza@gmail.com>
2025-08-28 17:35:01 +0300

    yaml setup with configuration sources

--- film-catalog/core/config.py
+++ film-catalog/core/config.py
@@ -3,7 +3,12 @@ from pathlib import Path
 from typing import Literal, Self
 
 from pydantic import BaseModel, model_validator
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    YamlConfigSettingsSource,
+)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -68,7 +73,43 @@ class Settings(BaseSettings):
         ),
         env_prefix="FILM_CATALOG__",
         env_nested_delimiter="__",
+        yaml_file=(
+            BASE_DIR / "config.default.yaml",
+            BASE_DIR / "config.local.yaml",
+        ),
+        yaml_config_section="film-catalog",
     )
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        """
+        Define the sources and their order for loading the settings values.
+
+        Args:
+            settings_cls: The Settings class.
+            init_settings: The `InitSettingsSource` instance.
+            env_settings: The `EnvSettingsSource` instance.
+            dotenv_settings: The `DotEnvSettingsSource` instance.
+            file_secret_settings: The `SecretsSettingsSource` instance.
+
+        Returns:
+        A tuple containing the sources and their order for loading the settings values.
+        """
+        return (
+            init_settings,  # дефолты имеют самый низкий приоритет
+            env_settings,  # переменные окружения переопределяют все кроме init_settings
+            dotenv_settings,  # .env переопределяет все кроме того что выше
+            file_secret_settings,  # временно не используем
+            YamlConfigSettingsSource(settings_cls),  # все кроме того, что выше
+        )
+
     logging: LoggingConfig = LoggingConfig()
     redis: RedisConfig = RedisConfig()
 
