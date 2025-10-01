52b1d748ecd3858f1a9961b18b85dd00f8af8f39
Yaroslav <iaroslav.bierioza@gmail.com>
2025-10-01 13:21:46 +0300

    Connection CSS

--- film-catalog/main.py
+++ film-catalog/main.py
@@ -3,11 +3,12 @@ import logging
 from fastapi import (
     FastAPI,
 )
+from fastapi.staticfiles import StaticFiles
 
 from api import router as api_router
 from api.main_views import router as main_router
 from app_lifespan import lifespan
-from core.config import settings
+from core.config import BASE_DIR, settings
 
 logging.basicConfig(
     level=settings.logging.log_level,
@@ -19,6 +20,8 @@ app = FastAPI(
     title="Film Catalog",
     lifespan=lifespan,
 )
+
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 
 app.include_router(api_router)
 app.include_router(main_router)
