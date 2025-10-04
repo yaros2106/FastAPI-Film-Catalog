83ff76f4663c7428d780a560ece6177b727834aa
Yaroslav <iaroslav.bierioza@gmail.com>
2025-10-04 14:21:32 +0300

    create active link

--- film-catalog/api/main_views.py
+++ film-catalog/api/main_views.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from fastapi import (
     APIRouter,
     Request,
@@ -15,13 +13,25 @@ router = APIRouter(
 
 @router.get(
     "/",
+    name="home",
     response_class=HTMLResponse,
     include_in_schema=False,
 )
-def read_root(request: Request) -> HTMLResponse:
-    context: dict[str, Any] = {}
+def home_page(request: Request) -> HTMLResponse:
     return templates.TemplateResponse(
         request=request,
         name="home.html",
-        context=context,
+    )
+
+
+@router.get(
+    "/about/",
+    name="about",
+    response_class=HTMLResponse,
+    include_in_schema=False,
+)
+def about_page(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse(
+        request=request,
+        name="about.html",
     )
