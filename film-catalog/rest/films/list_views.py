215ac82b9310bcf0d345db7d2d5243d983f44f99
Yaroslav <iaroslav.bierioza@gmail.com>
2025-10-05 20:25:40 +0300

    show films list table

--- film-catalog/rest/films/list_views.py
+++ film-catalog/rest/films/list_views.py
@@ -1,9 +1,15 @@
+from typing import TYPE_CHECKING, Any
+
 from fastapi import APIRouter, Request
 from starlette.responses import HTMLResponse
 
+from dependencies.films import GetFilmStorage
 from templating import templates
 
 router = APIRouter()
+
+if TYPE_CHECKING:
+    from schemas.film import Film
 
 
 @router.get(
@@ -11,8 +17,17 @@ router = APIRouter()
     name="films:list",
     response_class=HTMLResponse,
 )
-def list_view(request: Request) -> HTMLResponse:
+def list_view(
+    request: Request,
+    storage: GetFilmStorage,
+) -> HTMLResponse:
+    context: dict[str, Any] = {}
+    films: list[Film] = storage.get()
+    context.update(
+        films=films,
+    )
     return templates.TemplateResponse(
         request=request,
         name="films/list.html",
+        context=context,
     )
