ad79f394438fda88beec7d6d90426041f634fab1
Yaroslav <iaroslav.bierioza@gmail.com>
2025-10-14 16:36:13 +0300

    handle film update

--- film-catalog/rest/films/update_views.py
+++ film-catalog/rest/films/update_views.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Request
-from starlette.responses import HTMLResponse
+from fastapi import APIRouter, Request, status
+from starlette.responses import HTMLResponse, RedirectResponse
 
-from dependencies.films import FilmBySlug
+from dependencies.films import FilmBySlug, GetFilmStorage
 from schemas.film import FilmUpdate
 from services.films import FormResponseHelper
 
@@ -28,4 +28,27 @@ async def get_page_update_film(
         request=request,
         form_data=film_update,
         film=film,
+    )
+
+
+@router.post(
+    "/",
+    name="films:update",
+    response_model=None,
+)
+async def update_film(
+    request: Request,
+    storage: GetFilmStorage,
+    film: FilmBySlug,
+) -> RedirectResponse | HTMLResponse:
+    async with request.form() as form:
+        film_update = FilmUpdate.model_validate(form)
+
+    storage.update(
+        film=film,
+        film_in=film_update,
+    )
+    return RedirectResponse(
+        url=request.url_for("films:list"),
+        status_code=status.HTTP_303_SEE_OTHER,
     )
